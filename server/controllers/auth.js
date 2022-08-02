5cc14f390c946c9e4e01bf229c18df8e77795499
Dean <carmidean@gmail.com>
2022-08-02 23:15:56 +0300

    Migrated auth function to model

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -45,35 +45,6 @@ const forgotPassword = async (req, res) => {
   const { email } = req.body;
 
   res.json(await modelAuth.forgotPassword(email))
-
-  // MOVE TO MODEL
-  // find user by email
-  const user = await User.findOne({ email });
-  console.log("USER ===> ", user);
-  if (!user) {
-    return { error: "User not found" };
-  }
-  // generate code
-  const resetCode = nanoid(5).toUpperCase();
-  // save to db
-  user.resetCode = resetCode;
-  user.save();
-  // prepare email
-  const emailData = {
-    from: process.env.EMAIL_FROM,
-    to: user.email,
-    subject: "Password reset code",
-    html: "<h1>Your password  reset code is: {resetCode}</h1>"
-  };
-  // send email
-  try {
-    const data = await sgMail.send(emailData);
-    console.log(data);
-    return data
-  } catch (err) {
-    console.log(err);
-    return { 'error': 'failed to send reset email' };
-  }
 };
 
 const resetPassword = async function(req, res){
@@ -82,20 +53,6 @@ const resetPassword = async function(req, res){
 
     res.json(await modelAuth.resetPassword(email, password, resetCode))
 
-    //MOVE TO MODEL
-    // find user based on email and resetCode
-    const user = await User.findOne({ email, resetCode });
-    // if user not found
-    if (!user) {
-      return { error: "Email or reset code are invalid" };
-    }
-    
-    // hash password
-    const hashedPassword = await hashPassword(password);
-    user.password = hashedPassword;
-    user.resetCode = "";
-    user.save();
-    return {};
   } catch (err) {
     console.log(err);
   }
--- server/model/auth.js
+++ server/model/auth.js
@@ -3,61 +3,147 @@
 const db_name = "Noisy";
 var { emailExists } = require("./fetching_queries");
 var { insertUser } = require("./insert_queries");
-const { hashPassword, comparePassword } = require('../helpers/auth')
+const { hashPassword, comparePassword } = require("../helpers/auth");
 const jwt = require("jsonwebtoken");
 const { MongoConnection } = require("./mongoUtils");
+const nanoid = require("nanoid");
+// sendgrid
+require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_KEY);
 
-
-async function signUp( _email, _name, _dob, password) {
-
+async function signUp(_email, _name, _dob, password) {
   const hashedPassword = await hashPassword(password);
-  let userdb = await insertUser(MongoConnection, _name, _dob, _email,hashedPassword);
+  let userdb = await insertUser(
+    MongoConnection,
+    _name,
+    _dob,
+    _email,
+    hashedPassword
+  );
 
-  if (userdb.res == false){
-    return { "error": userdb.err}
+  if (userdb.res == false) {
+    return { error: userdb.err };
   }
 
-// create signed token
-const token = jwt.sign({ _id: userdb._id }, process.env.JWT_SECRET, {
-  expiresIn: "7d",
-});
- 
-  let userDetails = {"name":_name, "dob": _dob, "Email": _email, "userID":userdb.userID}
-  return {token, userDetails}
+  // create signed token
+  const token = jwt.sign({ _id: userdb._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  let userDetails = {
+    name: _name,
+    dob: _dob,
+    Email: _email,
+    userID: userdb.userID,
+  };
+  return { token, userDetails };
 }
 
-
-async function signIn( _email, plain_password) {
+async function signIn(_email, plain_password) {
   let uid = "";
   let inDB = await emailExists(MongoConnection, _email);
   if (inDB == false) {
-    return {"error": "Email doesn't exist in database"}
-  } 
-    // check the hashed password is equal to hash in db
-    const doc = await MongoConnection.db(db_name)
-      .collection("users")
-      .findOne({ Email: _email });
+    return { error: "Email doesn't exist in database" };
+  }
+  // check the hashed password is equal to hash in db
+  const doc = await MongoConnection.db(db_name)
+    .collection("users")
+    .findOne({ Email: _email });
 
-      const match = await comparePassword(plain_password, doc.password);
-      if (!match) {
-        return {
-          "error": "Wrong password",
-        };
-      }
-    
-      // passwords (hashes) are equal, correct password
-      uid = doc._id.toString();
-      
-      const token = jwt.sign({ _id: uid }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-      });
-      doc.password = undefined;
-      // doc.secret = undefined;
-      return {
-        token,
-        doc,
-      };
+  const match = await comparePassword(plain_password, doc.password);
+  if (!match) {
+    return {
+      error: "Wrong password",
+    };
+  }
+
+  // passwords (hashes) are equal, correct password
+  uid = doc._id.toString();
+
+  const token = jwt.sign({ _id: uid }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  doc.password = undefined;
+  // doc.secret = undefined;
+  return {
+    token,
+    doc,
+  };
 }
 
-exports.signIn = signIn
-exports.signUp = signUp
+async function forgotPassword(_email) {
+  // add a field of
+  let dbUser = await MongoConnection.db(db_name)
+    .collection("users")
+    .findOne({ Email: _email });
+  if (!dbUser) {
+    return { error: "Email not found" };
+  }
+  const resetCode = nanoid(5).toUpperCase();
+  const filter = { Email: _email };
+  const expirationDate = new Date();
+  console.log(expirationDate.toString());
+  expirationDate.setMinutes(expirationDate.getMinutes() + 15);
+  console.log(expirationDate.toString());
+  const update = {
+    $set: { resetCode: resetCode, expirationDate: expirationDate },
+  };
+
+  const result = await await MongoConnection.db(db_name)
+    .collection("users")
+    .updateOne(filter, update);
+  result.then(() => {
+    console.log("Expiration Date: " + expirationDate);
+  });
+
+  // prepare email
+  const emailData = {
+    from: process.env.EMAIL_FROM,
+    to: dbUser.Email,
+    subject: "Password reset code",
+    html: "<h1>Your password  reset code is: {resetCode}</h1>",
+  };
+  // send email
+  try {
+    const data = await sgMail.send(emailData);
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+    return { error: "failed to send reset email" };
+  }
+}
+
+async function resetPassword(_email, _password, _resetCode) {
+  const dbUser = await MongoConnection.db(db_name).collection.findOne({
+    Email: _email,
+    resetCode: _resetCode,
+  });
+  if (!dbUser) {
+    // Not found
+    return { error: "failed to find email that matches reset code" };
+  }
+  // How many minutes passed  since the expiration date
+  const minutesPassed = (new Date() - dbUser.expirationDate) / 60000;
+
+  if (minutesPassed < 0) {
+    // change password to new password
+    // set reset code to an empty string
+    const filter = { Email: _email };
+    const hashPass = await hashPassword(_password);
+    const update = { $set: { password: hashPass, resetCode: "" } };
+
+    const passChange = await MongoConnection.db(db_name)
+      .collection("users")
+      .updateOne(filter, update);
+    return { res: "Password changed successfully" };
+  } else {
+    return { error: "Reset code expired" };
+  }
+}
+
+exports.signIn = signIn;
+exports.signUp = signUp;
+exports.resetPassword = resetPassword;
+exports.forgotPassword = forgotPassword;
