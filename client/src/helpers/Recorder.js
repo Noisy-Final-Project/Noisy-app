c7fcac0615ff0973d7f14439f4c28a2134bdc2d4
Dean <carmidean@gmail.com>
2022-08-10 13:36:19 +0300

    recorder and api.org

--- client/src/helpers/Recorder.js
+++ client/src/helpers/Recorder.js
@@ -1,45 +1,50 @@
 import { useState } from "react";
-let decibels = []
+let decibels = [];
 const [recording, setRecording] = React.useState(false);
-  
-  const onRecordingStatusUpdate = playbackStatus => {
-    console.log('Current status: '+JSON.stringify(playbackStatus))
-  };
-  
-  async function startRecording() {
-    try {
-      console.log(JSON.stringify(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY));
-      console.log('Requesting permissions..');
-      await Audio.requestPermissionsAsync();
-      await Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        playsInSilentModeIOS: true,
-      }); 
-      console.log('Starting recording..');
-      const { recording, status } = await Audio.Recording.createAsync(
-        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY,
-        onRecordingStatusUpdate,
-        200
-      );
-      // milliseconds of recording
-      const timeMillis = 15000
 
-      console.log('Init Recording status: '+status)
-      decibels.push(status.metering);
-      setRecording(recording);
-      console.log('Recording started');
-      // stops recording after 
-    setTimeout(stopRecording, timeMillis );
-    return
-    } catch (err) {
-      console.error('Failed to start recording', err);
-    }
-  }
+const onRecordingStatusUpdate = (playbackStatus) => {
+  console.log("Current status: " + JSON.stringify(playbackStatus));
+  decibels.push(playbackStatus.metering);
+};
 
-  async function stopRecording() {
-    console.log('Stopping recording..');
-    setRecording(undefined);
-    await recording.stopAndUnloadAsync();
-    const uri = recording.getURI(); 
-    console.log('Recording stopped and stored at', uri);
+async function startRecording() {
+  try {
+    // TODO make sure this line empties the array in the beginning of each recording
+    decibels.empty();
+    // TODO how to make the array decibels exported to other screens
+    console.log(JSON.stringify(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY));
+    console.log("Requesting permissions..");
+    await Audio.requestPermissionsAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: true,
+      playsInSilentModeIOS: true,
+    });
+    console.log("Starting recording..");
+    const timeMillis = 15000;
+    const intervals = 200;
+    const { recording, status } = await Audio.Recording.createAsync(
+      Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY,
+      onRecordingStatusUpdate,
+      intervals
+    );
+    // milliseconds of recording
+
+    console.log("Init Recording status: " + status);
+    setRecording(recording);
+    console.log("Recording started");
+    // stops recording after
+    setTimeout(stopRecording, timeMillis);
+    return;
+  } catch (err) {
+    console.error("Failed to start recording", err);
   }
+}
+
+async function stopRecording() {
+  // TODO the audio file is not needed, therefore should be deleted.
+  console.log("Stopping recording..");
+  setRecording(undefined);
+  await recording.stopAndUnloadAsync();
+  const uri = recording.getURI();
+  console.log("Recording stopped and stored at", uri);
+}
